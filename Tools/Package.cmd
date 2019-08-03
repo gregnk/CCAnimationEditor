c7020806e317757668974780ddeea736ede7eb2d
Gregory Karastergios <gregory@gregnk.com>
2019-08-03 14:06:46 -0400

    Added package script

---
+++ Tools/Package.cmd
@@ -0,0 +1,6 @@
+@echo off
+set /p ver="Enter version: v"
+cd ..\CCAnimationEditor\bin\Release
+::del CCAnimationEditor\bin\Release\Settings.xml
+"C:\Program Files\7-Zip\7z.exe" a D:\Projects\GameToolProjects\CCAnimationEditor\Distrib\CCAnimationEditor-v%ver%.7z
+pause
