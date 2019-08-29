f929b2065455874e7965e463158ae821460f2458
Gregory Karastergios <gregory@gregnk.com>
2019-08-29 13:24:40 -0400

    Changed Package.cmd to output to zip instead of 7z

--- Tools/Package.cmd
+++ Tools/Package.cmd
@@ -2,5 +2,5 @@
 set /p ver="Enter version: v"
 cd ..\CCAnimationEditor\bin\Release
 ::del CCAnimationEditor\bin\Release\Settings.xml
-"C:\Program Files\7-Zip\7z.exe" a D:\Projects\GameToolProjects\CCAnimationEditor\Distrib\CCAnimationEditor-v%ver%.7z
+"C:\Program Files\7-Zip\7z.exe" a D:\Projects\GameToolProjects\CCAnimationEditor\Distrib\CCAnimationEditor-v%ver%.zip
 pause
